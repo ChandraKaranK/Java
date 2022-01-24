28ba07f66e8aee0c15663d83e58fe84d7a844eb5
Chandrakaran Reddy <chandrakaran.kethireddy@zemosolabs.com>
2022-01-24 11:30:45 +0530

    Changes made in assignment3 Ping.java

--- src/com/JavaAssignment/assignment3/Ping.java
+++ src/com/JavaAssignment/assignment3/Ping.java
@@ -10,29 +10,30 @@ import java.util.regex.*;
 
 public class Ping {
 
-    public final int no_of_packets = 4;
-    public final int first_reply_index = 2;
+    public final int noFPackets = 4;
+    public final int firstReplyIndex = 2;
 
-    public void ping(String cmnd) throws IOException
-    {
+    public void ping(String command) throws IOException {
         ArrayList<String> output = new ArrayList<>();
         String line;
-        Process p = Runtime.getRuntime().exec(cmnd);
+        Process p = Runtime.getRuntime().exec(command);
         BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-        while((line=input.readLine())!=null) {
-            System.out.println(line);
+        while ((line = input.readLine()) != null) {
             output.add(line);
         }
         input.close();
 
         ArrayList<String> replies = new ArrayList<>();
 
-        for (int reply_line_index = first_reply_index; reply_line_index < 2 + no_of_packets; reply_line_index++) {
-            replies.add(output.get(reply_line_index));
-            System.out.println(output.get(reply_line_index));
+        for (int ReplyIndex = this.firstReplyIndex; ReplyIndex < 2 + noFPackets; ReplyIndex++) {
+            replies.add(output.get(ReplyIndex));
+            System.out.println(output.get(ReplyIndex));
         }
+        findMedian(replies);
+    }
 
+    void findMedian(ArrayList<String> replies){
         ArrayList<Integer> times = new ArrayList<>();
 
         for(String reply:replies)
@@ -42,17 +43,14 @@ public class Ping {
             if(m.find())
             {
                 String timestamp = m.group();
-                String time_in_ms = timestamp.substring(5);
-                times.add(Integer.parseInt(time_in_ms));
+                String timeInMs = timestamp.substring(5);
+                times.add(Integer.parseInt(timeInMs));
             }
         }
 
-        for(int t:times)
-            System.out.println(t);
-
         Collections.sort(times);
         double median = (times.get(1)+times.get(2))/(2.0);
-        System.out.println("Median time for the response is "+median );
+        System.out.println("\nMedian time for the response is "+median );
 
     }
 
@@ -62,8 +60,8 @@ public class Ping {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter ip address");
         String ip = sc.nextLine();
-        String cmnd = "ping " + ip;
-        p.ping(cmnd);
+        String command = "ping " + ip;
+        p.ping(command);
 
     }
 }
